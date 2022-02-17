dc48328e793f992ad07ca96b0570cde317cdc8c0
Cody <relationalprogrammer@gmail.com>
2022-02-17 11:51:06 -0600

    styled some auth buttons

--- src/components/Static/Header.jsx
+++ src/components/Static/Header.jsx
@@ -5,6 +5,10 @@ export default function Header() {
     <header className="banner">
         <div className="banner-title">Name Rater</div>
         <img className="logo" src="/images/logo.png" alt="app logo" />
+        <div className="flex-cont" style={{marginLeft: "2%", width:"10%", justifyContent:"space-around"}}>
+          <button className="signup auth-btn">Sign Up</button>
+          <button className="login auth-btn">Log In</button>
+        </div>
     </header>
   )
 }
