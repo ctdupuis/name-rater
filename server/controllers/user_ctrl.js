1f208140d19f0d29816e0b70fa8d1a8ce727f539
Cody <relationalprogrammer@gmail.com>
2022-02-17 14:28:52 -0600

    set up some user controller actions & routes

--- server/controllers/user_ctrl.js
+++ server/controllers/user_ctrl.js
@@ -4,7 +4,26 @@ const User = require('../models/user');
 const salt = bcrypt.genSaltSync(10);
 
 module.exports = {
-    register: (req, res) => {
-
+    auth: async(req, res) => {
+        if (req.session.user) {
+            res.status(200).send({ auth: req.session.user })
+        } else {
+            res.status(200).send({ auth: false })
+        }
+    },
+    register: async(req, res) => {
+        const { userdata } = req.body;
+        const securePass = bcrypt.hashSync(userdata.password, salt);
+        userdata.password = securePass;
+        const newUser = new User(userdata);
+        try {
+            const insertion = await User.create(newUser);
+            const secureUser = {...insertion["_doc"]};
+            delete secureUser.password;
+            req.session.user = secureUser;
+            res.status(200).send(secureUser);
+        } catch (err) {
+            res.status(400).send({ alert: { type: 'error', message: "Username or email is already taken "}});
+        }
     }
 };
--- server/routes/user_routes.js
+++ server/routes/user_routes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const {
+    auth,
     register
 } = require("../controllers/user_ctrl");
 
+router.get("/auth", auth);
 router.post("/register", register)
 
 module.exports = router;
