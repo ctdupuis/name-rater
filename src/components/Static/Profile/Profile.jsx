059ef1a9f32fafd4539a83c3d77065d47c137d96
Cody <relationalprogrammer@gmail.com>
2022-02-18 14:23:44 -0600

    animated logo

--- src/components/Static/Profile/Profile.jsx
+++ src/components/Static/Profile/Profile.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import "../../../stylesheets/profile.css";
 
-export default function Profile() {
+export default function Profile({ currentUser }) {
   return (
-    <div>Da Profile Page</div>
+    <div className="wrapper">
+        Da Profile Page
+    </div>
   )
 }
